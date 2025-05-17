1714552fc4b66f9ae5e83b58324b76f793176686
vatan45 <vmro45@gmail.com.com>
2025-05-17 23:25:31 +0530

    initial commits

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ passport.deserializeUser(async (id, done) => {
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: 'http://localhost:5001/auth/google/callback'
+  callbackURL: 'https://truck-xddp.onrender.com/auth/google/callback'
 },
   async (accessToken, refreshToken, profile, done) => {
     try {
